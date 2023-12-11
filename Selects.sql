c13c005828b0200b844fc6b2e0c0a03cee7dc347
lucijs <fradelic.lucija@gmail.com>
2023-12-11 23:59:43 +0100

    final changes

--- Selects.sql
+++ Selects.sql
@@ -3,10 +3,7 @@ SELECT * FROM Tickets  WHERE Price >=100 AND Price <=200;
 SELECT * FROM  Employes WHERE Gender='F'  AND (SELECT COUNT(*) FROM EmployesOnAFlight f WHERE f.EmployeId= EmployeId AND f.FlightId IN (SELECT FlightId FROM Flights WHERE Departure<'2023-12-10'))>20;
 SELECT * FROM  Employes WHERE (SELECT COUNT(*) FROM EmployesOnAFlight e WHERE e.EmployeId=EmployeId AND e.FlightId IN (SELECT FlightId FROM Flights WHERE Departure<'2023-12-11' AND (Departure::timestamp + Duration)>'2023-12-11'))>0 AND Role=2;
 SELECT * FROM  Flights WHERE ((SELECT AirportId FROM Airports WHERE CityId = (SELECT CityId FROM Cities WHERE Name = 'Split'))=WhereTo or (SELECT AirportId FROM Airports WHERE CityId = (SELECT CityId FROM Cities WHERE Name = 'Split'))=WhereFrom) and Departure<'2024-01-01' AND Departure>'2022-01-01'; 
-
-
 SELECT* FROM  Flights WHERE WhereTo IN (SELECT AirportID FROM Airports WHERE CityId = (SELECT CityId FROM Cities WHERE Name = 'Vienna'))  AND Departure<'2024-01-01' AND Departure>'2023-11-30';
-
 
 SELECT COUNT(*) FROM Tickets WHERE Type = 2 and FlightId IN(select flightid from flights WHERE departure>='2021-01-01' and departure<='2022-01-01' and airplaneid=(select airplaneid from airplanes where companyid =(select companyid from companies where name = 'AirDUMP') ));
 SELECT avg(Number) FROM grade WHERE ticketid =(select ticketid from tickets WHERE flightid=(select flightid from flights where airplaneid=(select airplaneid from airplanes where companyid = (select companyid from companies where name='AirDUMP'))));
@@ -19,4 +16,10 @@ order by count((select companyid from company where name='AirBus')=(select compa
 							  or(whereto=a.cityid
 								 and extract(epoch from(now()-arrival))/60=0))))
 
-select * from airport where (sqrt(power()))	
+select * from airports  a where (SELECT sqrt(power(c.Latitude::float -43.5147::float,2)+power(c.Latitude::float -16.4435::float,2)) FROM Cities c WHERE c.CityId = a.CityId)<1500;
+
+select Price*0.8 from Tickets t where  (select count(*) from flights f where f.FlightId=t.FlightId)<20;
+
+update  Airplanes
+	set StateId = 4
+	where (select count(*) from flights f where airplaneId = f.AirplaneId and Departure<'2023-12-11')=0 and NOW()-Produced<interval(hour := 20*24*365.25);
